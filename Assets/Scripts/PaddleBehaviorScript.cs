42dcb74acbe2081f565413a804afb19f6727d1e0
Cade Gordon <cadegordon093@gmail.com>
2022-05-02 16:46:19 -0500

    made paddles movment and map layout

---
+++ Assets/Scripts/PaddleBehaviorScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBehaviorScript : MonoBehaviour
+{
+    [SerializeField]
+    private float _restPosition = 0f;
+    [SerializeField]
+    private float _pressedPosition = 45f;
+    [SerializeField]
+    private float _hitStrength = 10000f;
+    [SerializeField]
+    private float _flipperDamper = 150f;
+
+    public string _inputName;
+    HingeJoint _hinge;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _hinge = GetComponent<HingeJoint>();
+        _hinge.useSpring = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JointSpring spring = new JointSpring();
+        spring.spring = _hitStrength;
+        spring.damper = _flipperDamper;
+
+        if (Input.GetAxis(_inputName) == 1)
+        {
+            spring.targetPosition = _pressedPosition;
+        }
+        else
+        {
+            spring.targetPosition = _restPosition;
+        }
+        _hinge.spring = spring;
+        _hinge.useLimits = true;
+    }
+}
