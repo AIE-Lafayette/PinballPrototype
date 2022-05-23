b9ec2cb66e00efaaa025120c61d3083f20696eea
Brittnie <brittniesexton95@gmail.com>
2022-05-23 10:20:09 -0500

    Quit button stops game in pause menu

---
+++ Assets/Scripts/PauseMenuBehaviour.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuBehaviour : MonoBehaviour
+{
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
