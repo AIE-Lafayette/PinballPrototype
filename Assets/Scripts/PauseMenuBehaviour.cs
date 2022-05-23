489a76e3ed9eca12a72b230f07b5445a7853a784
Brittnie <brittniesexton95@gmail.com>
2022-05-23 11:19:51 -0500

    Worked on pause menu

--- Assets/Scripts/PauseMenuBehaviour.cs
+++ Assets/Scripts/PauseMenuBehaviour.cs
@@ -1,32 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuBehaviour : MonoBehaviour
 {
-    public void PauseGame()
+    [SerializeField]
+    private GameObject _pauseMenu;
+    private bool _isPaused;
+
+    /// <summary>
+    /// The pause menu is activated and time has stopped.
+    /// </summary>
+    public void ActivateMenu()
     {
+        //Game is paused
+        _pauseMenu.SetActive(true);
         Time.timeScale = 0;
     }
 
-    public void ResumeGame()
+    /// <summary>
+    /// The pause menu is no longer active and time as resumed.
+    /// </summary>
+    public void DeactivateMenu()
     {
+        _pauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
 
+    /// <summary>
+    /// Loads the main game and restarts the game completely
+    /// </summary>
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(2);
+    }
+
+    /// <summary>
+    /// The game closes out completely
+    /// </summary>
     public void QuitGame()
     {
         Application.Quit();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //if(Input.GetKeyDown(KeyCode.P))
+        //{
+        //    _isPaused = true;
+        //}
+
+        if (_isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DeactivateMenu();
+        }
     }
 }
