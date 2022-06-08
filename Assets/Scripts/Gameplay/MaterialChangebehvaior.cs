57a9a574c3c85f73788d4ebca0b952b9b917a3cc
Cade Gordon <cadegordon093@gmail.com>
2022-06-08 11:10:32 -0500

    rune now lights up on collision

---
+++ Assets/Scripts/Gameplay/MaterialChangebehvaior.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChangebehvaior : MonoBehaviour
+{
+    public Material[] _material;
+    private Renderer _render;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _render = GetComponent<Renderer>();
+        _render.enabled = true;
+        _render.sharedMaterial = _material[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            _render.sharedMaterial = _material[1];
+        }
+        
+        
+
+        
+    }
+}
--- Assets/Scripts/Gameplay/ScoreMultiplierBehavior.cs
+++ Assets/Scripts/Gameplay/ScoreMultiplierBehavior.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ScoreMultiplierBehavior : MonoBehaviour
 {
+
+
+
     // Start is called before the first frame update
     void Start()
     {
